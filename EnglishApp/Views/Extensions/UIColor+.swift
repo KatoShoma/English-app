0234a465008c3a93cb39edc0f6b76621adc8da9e
Rena-Koura <107632016+Rena-Koura@users.noreply.github.com>
2022-08-20 21:46:47 +0900

    fix:設定画面の背景色変更

--- EnglishApp/Views/Extensions/UIColor+.swift
+++ EnglishApp/Views/Extensions/UIColor+.swift
@@ -10,18 +10,36 @@ import UIKit
 extension UIColor {
 
     // 以下で拡張色の定義
-    // 赤
-    static let pastelRed = UIColor(hex: "#FF9999")
     // 黒
     static let lightBlack = UIColor(hex: "#323232")
-    // オレンジ
-    static let sandyBrown = UIColor(hex: "#FFA463")
-    // 青
-    static let safeColor = UIColor(hex: "1FA5DF")
+    // 赤
+    static let pastelRed = UIColor(hex: "#FF9999")
+    // 背景(白)
+    static let backGround = UIColor(hex: "#FFFFFF")
+    // 画面下部の白みの強いグレー
+    static let lightGray = UIColor(hex: "#F5F4F4")
+    // 調理開始
+    static let startOrange = UIColor(hex: "#DF7756")
+    // 時間の設定
+    static let timerBlue = UIColor(hex: "#ACCDFF")
+    // タイトルに戻る
+    static let returnGray = UIColor(hex: "#C4C4C4")
+    // ドラムピッカー・苦手単語・学習回数・最高正答数
+    static let timerGray = UIColor(hex: "#D9D9D9")
+    // ランクボタン
+    static let rankCream = UIColor(hex: "#FFEAD1")
+    // 保存・キャンセルボタン・ランクとは
+    static let lightBlue = UIColor(hex: "#0AC4FF")
+    // ゴールド
+    static let gold = UIColor(hex: "#BFAB40")
     // シルバー
-    static let silver = UIColor(hex: "#C4C4C4")
+    static let silver = UIColor(hex: "#7C7979")
+    // ブロンズ
+    static let bronze = UIColor(hex: "#DF7756")
+    // 設定
+    static let settingPink = UIColor(hex: "FFF4F9")
 
-    //  色をhexで編集できるように修正
+    // 色をhexで編集できるように修正
     convenience init?(hex: String, alpha: CGFloat = 1.0) {
         let validatedHexColorCode = hex.replacingOccurrences(of: "#", with: "")
         let scanner = Scanner(string: validatedHexColorCode)
