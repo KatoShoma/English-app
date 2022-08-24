884c130aa610359847d564687005b839f4162259
Rena-Koura <107632016+Rena-Koura@users.noreply.github.com>
2022-08-24 23:13:04 +0900

    add:キャンセル・保存ボタン

--- EnglishApp/Views/Study/SetTimeViewController.swift
+++ EnglishApp/Views/Study/SetTimeViewController.swift
@@ -17,7 +17,30 @@ class SetTimeViewController: UIViewController {
         hosting.didMove(toParent: self)
         hosting.view.equalConstraintTo(view)
         title = "時間の設定"
-        // キャンセル・保存ボタン navigationViewController(?)
+    }
+
+    // キャンセル・保存ボタン
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var cancelButtonItem: UIBarButtonItem! // キャンセルボタン
+        var preservationButtonItem: UIBarButtonItem! // 保存ボタン
+
+        cancelButtonItem = UIBarButtonItem(title: "キャンセル", style: .plain, target: self, action: #selector(cancelButtonPressed(_:)))
+        preservationButtonItem = UIBarButtonItem(title: "保存", style: .plain, target: self, action: #selector(preservationButtonPressed(_:)))
+
+        cancelButtonItem.tintColor = UIColor.lightBlue
+        preservationButtonItem.tintColor = UIColor.lightBlue
+
+        self.navigationItem.leftBarButtonItem = cancelButtonItem
+        self.navigationItem.rightBarButtonItem = preservationButtonItem
+    }
+
+    @objc func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        print("追加ボタンが押されました")
+    }
+
+    @objc func preservationButtonPressed(_ sender: UIBarButtonItem) {
+        print("削除ボタンが押されました")
     }
 
     required init?(coder: NSCoder) {
