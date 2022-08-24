1288ff42539933eec434eaae763afe443622f87e
ShomaSon <x17032xx@gmail.com>
2022-08-24 12:00:41 +0900

    add: OutcomePresenter's file

---
+++ EnglishApp/Presenters/OutcomePresenter.swift
@@ -0,0 +1,26 @@
+//
+//  OutcomePresenter.swift
+//  EnglishApp
+//
+//  Created by 加藤祥真 on 2022/08/24.
+//
+
+protocol OutcomePresenterProtocol {
+    func studyDidTap()
+    func timeDidTap()
+}
+
+final class OutcomePresenter: OutcomePresenterProtocol {
+    
+    init(){
+        
+    }
+    
+    func studyDidTap() {
+        
+    }
+    
+    func timeDidTap() {
+        
+    }
+}
