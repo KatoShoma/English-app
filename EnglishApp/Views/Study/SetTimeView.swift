c6b56cfe34285f4e1e99a2bd1da1e408f97272ea
Rena-Koura <107632016+Rena-Koura@users.noreply.github.com>
2022-08-26 08:39:23 +0900

    fix:ドラムロールの選択肢

--- EnglishApp/Views/Study/SetTimeView.swift
+++ EnglishApp/Views/Study/SetTimeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SetTimeView: View {
     @State private var selection = 0
-    let selections = ["1", "2", "3", "4", "5"]
+    let selections = ["1分", "2分", "3分", "4分", "5分"]
     var body: some View {
         Picker(selection: $selection, label: Text("時間の選択(分)")) {
             ForEach(0..<5) { index in
