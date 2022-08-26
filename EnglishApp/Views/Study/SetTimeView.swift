851ef9766f1798f42e71c75e122570c971bee8f8
Rena-Koura <107632016+Rena-Koura@users.noreply.github.com>
2022-08-26 20:50:52 +0900

    fix:Pickerを含む全体をVStackで囲み、Spacerを入れる

--- EnglishApp/Views/Study/SetTimeView.swift
+++ EnglishApp/Views/Study/SetTimeView.swift
@@ -11,15 +11,15 @@ struct SetTimeView: View {
     @State private var selection = 0
     let selections = ["1分", "2分", "3分", "4分", "5分"]
     var body: some View {
-        Picker(selection: $selection, label: Text("時間の選択(分)")) {
-            ForEach(0..<5) { index in
-                Text(self.selections[index])
-            }
-        }
-        .labelsHidden()
-        .background(Color(UIColor.timerGray))
-        .pickerStyle(WheelPickerStyle())
         VStack {
+            Picker(selection: $selection, label: Text("時間の選択(分)")) {
+                ForEach(0..<5) { index in
+                    Text(self.selections[index])
+                }
+            }
+            .labelsHidden()
+            .background(Color(UIColor.timerGray))
+            .pickerStyle(WheelPickerStyle())
             Spacer()
         }
     }
