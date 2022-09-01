845fc3aeef51f0395c4ac68777b01c316b40570e
Rena-Koura <107632016+Rena-Koura@users.noreply.github.com>
2022-09-01 10:01:12 +0900

    add:ランク

--- EnglishApp/Views/Data/DetailDataView.swift
+++ EnglishApp/Views/Data/DetailDataView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct DetailDataView: View {
+    final class Model: ObservableObject {
+        @Published var rank: String?
+        @Published var rankColor: Color?
+        @Published var weakWord: [String]?
+    }
+
     var body: some View {
-        Text("詳細データ")
+        GeometryReader { geometry in
+            let height = geometry.size.height
+            VStack {
+                (Text("ランク")
+                    .font(.system(size: 24))
+                    .foregroundColor(Color(UIColor.lightBlack))
+                    .padding(EdgeInsets(
+                        top: 24,
+                        leading: 146,
+                        bottom: 24,
+                        trailing: 146
+                    ))
+                )
+                .background(Color(UIColor.gold).cornerRadius(6))
+                .frame(maxWidth: .infinity, minHeight: CGFloat(height)/12)
+            }
+            Spacer()
+        }
     }
 }
 
